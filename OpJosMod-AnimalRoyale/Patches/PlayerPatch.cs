a3404c548e87a745119e95115f82ab9b668d4335
Jglauby <jglauby@gmail.com>
2024-06-26 19:50:36 -0500

    Add project files.

---
+++ OpJosMod-AnimalRoyale/Patches/PlayerPatch.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP.UnityEngine;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using UnityEngine;
+using KeyCode = BepInEx.Unity.IL2CPP.UnityEngine.KeyCode;
+
+namespace OpJosModAnimalRoyale.TestMod.Patches
+{ 
+    [HarmonyPatch(typeof(MonoBehaviourPublicObcuBousBoSiovBoCoCaUnique))]
+    internal class PlayerPatch
+    {
+        private static ManualLogSource mls;
+        public static void SetLogSource(ManualLogSource source)
+        {
+            mls = source;
+        }
+
+        private static bool alreadyPressed = false;
+        private static KeyCode key = KeyCode.C;
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        private static void UpdatePatch(MonoBehaviourPublicObcuBousBoSiovBoCoCaUnique __instance)
+        {
+            if (Input.GetKeyInt(key) && alreadyPressed == false) //show imposters
+            {
+                doThing(__instance);
+            }
+
+            alreadyPressed = Input.GetKeyInt(key);
+        }
+        private static void doThing(MonoBehaviourPublicObcuBousBoSiovBoCoCaUnique __instnace)
+        {
+            mls.LogInfo("button was pressed");
+
+            //__instnace.GameServerSentPlayerHPArmorMoveModeChange(1, false, 1, 1, 1, 1, 1, 1, 1);
+
+            __instnace.gameObject.transform.position += new Vector3(20, 20, 20);
+        }
+    }
+}
---
+++ OpJosMod-AnimalRoyale/Plugin.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using OpJosModAnimalRoyale.TestMod.Patches;
+
+namespace OpJosModAnimalRoyale.TestMod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class OpJosModAnimalRoyaleTestMod : BasePlugin
+    {
+        private const string modGUID = "OpJosModAnimalRoyale.TestMod";
+        private const string modName = "TestMod";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static OpJosModAnimalRoyaleTestMod Instance;
+
+        internal ManualLogSource mls;
+
+        //not needed?
+        public override void Load()
+        {
+            Loaded();
+        }
+
+        void Awake()
+        {
+            Loaded();
+        }
+
+        private void Loaded()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo(modName + " has loaded");
+
+            PlayerPatch.SetLogSource(mls);
+            harmony.PatchAll();
+        }
+    }
+}
